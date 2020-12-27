d1894ea1060851f576418c7d3353d9f805984d33
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-27 20:53:14 +0200

    #2 request new password and change password ui done.

---
+++ app/src/main/java/com/kagan/chatapp/ui/PasswordRequestFragment.kt
@@ -0,0 +1,14 @@
+package com.kagan.chatapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.kagan.chatapp.R
+
+class PasswordRequestFragment : Fragment(R.layout.fragment_password_request) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
