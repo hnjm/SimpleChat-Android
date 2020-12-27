2701a2b7527c447418c80915dc942ae96fa38c3e
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-27 20:16:03 +0200

    #1 RegisterFragment done.

--- app/src/main/java/com/kagan/chatapp/ui/RegisterFragment.kt
+++ app/src/main/java/com/kagan/chatapp/ui/RegisterFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.kagan.chatapp.R
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
