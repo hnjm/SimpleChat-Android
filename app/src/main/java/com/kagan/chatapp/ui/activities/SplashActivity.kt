b8b10fd83dd4ed834482ed85ee54c7a1022250ea
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-27 23:12:01 +0200

    #3 Splash start activity

--- app/src/main/java/com/kagan/chatapp/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/kagan/chatapp/ui/activities/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.kagan.chatapp.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.kagan.chatapp.R
+import kotlinx.coroutines.*
 
 const val TAG = "SplashActivity"
 
@@ -10,6 +13,26 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        CoroutineScope(Dispatchers.IO).launch {
+            val isLogin = isLogin()
+            withContext(Dispatchers.Main) {
+                if (isLogin) {
+                    Log.d(TAG, "onCreate: Main activity have not created yet")
+                } else {
+                    startActivity()
+                }
+            }
+        }
+    }
+
+    private fun startActivity() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+    private suspend fun isLogin(): Boolean {
+        delay(1000)
+        return false
     }
 
 }
