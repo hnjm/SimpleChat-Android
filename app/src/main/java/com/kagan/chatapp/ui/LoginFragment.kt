84723064e71837db584ed2b8ed3dcef95af2ee86
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-12-27 18:37:52 +0200

    LoginFragment, nav_graph created.

---
+++ app/src/main/java/com/kagan/chatapp/ui/LoginFragment.kt
@@ -0,0 +1,16 @@
+package com.kagan.chatapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kagan.chatapp.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
