a45e0c5f745750b32172d977d325962583a50f6f
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2021-01-10 20:10:07 +0200

    #21 Api Base Url. There are two one is for emulator, other for the real devices.

--- app/src/main/java/com/kagan/chatapp/utils/Constants.kt
+++ app/src/main/java/com/kagan/chatapp/utils/Constants.kt
@@ -2,7 +2,8 @@ package com.kagan.chatapp.utils
 
 class Constants {
 
-    companion object{
-        const val BASE_URL = ""
+    companion object {
+        const val BASE_URL = "https://10.0.2.2:5060/api/"
+//        const val BASE_URL = "https://192.168.0.104:5060/api/"
     }
 }
