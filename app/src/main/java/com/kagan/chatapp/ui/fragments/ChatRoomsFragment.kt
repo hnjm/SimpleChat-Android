3560cf5a377c3a039f4a4aeaeba790c7ef515ba6
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2021-01-19 21:24:34 +0200

    #32 Chat Rooms UI created.

---
+++ app/src/main/java/com/kagan/chatapp/ui/fragments/ChatRoomsFragment.kt
@@ -0,0 +1,17 @@
+package com.kagan.chatapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kagan.chatapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChatRoomsFragment : Fragment(R.layout.fragment_chat_rooms) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
