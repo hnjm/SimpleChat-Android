6cfb00105f460d7741fca0f0459895f9f6729a37
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2021-01-19 22:13:37 +0200

    #32 ChatRoomsApi functions and model

--- app/src/main/java/com/kagan/chatapp/api/ChatRoomsApi.kt
+++ app/src/main/java/com/kagan/chatapp/api/ChatRoomsApi.kt
@@ -1,4 +1,28 @@
 package com.kagan.chatapp.api
 
+import com.google.gson.JsonElement
+import com.kagan.chatapp.models.chatrooms.AddVM
+import retrofit2.Call
+import retrofit2.http.*
+import java.util.*
+
 interface ChatRoomsApi {
+
+    @GET("ChatRooms/")
+    fun getChatRooms(): Call<JsonElement>
+
+    @POST("ChatRooms/")
+    fun postChatRooms(@Body chatRoom: AddVM): Call<JsonElement>
+
+    @GET("ChatRooms/{id}/users")
+    fun getChatRoomUsers(@Path("id") id: UUID): Call<JsonElement>
+
+    @GET("ChatRooms/{id}/")
+    fun getChatRoom(@Path("id") id: UUID): Call<JsonElement>
+
+    @PUT("ChatRooms/{id}/")
+    fun putChatRoom(@Path("id") id: UUID, @Body chatRoom: AddVM): Call<JsonElement>
+
+    @DELETE("ChatRooms/{id}/")
+    fun deleteChatRoom(@Path("id") id: UUID): Call<JsonElement>
 }
